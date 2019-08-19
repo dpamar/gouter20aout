65d7c020610841d265165c4a491aeee76b1a6c62
dpamar <dpamar@gmail.com>
2019-08-19 22:50:10 +0200

    Update places.js

--- places.js
+++ places.js
@@ -1,3 +1,10 @@
 var places = [
+    [45.183418, 5.721562, 'Ça marche peut-être depuis ton bureau, mais il faudra sortir pour les suivants de toutes les façons ;-)'],
+    [45.183673, 5.721394, 'Bon ok, c est juste à côté je me suis pas foulé. Mais je t aime'],
+    [45.184194, 5.721877, 'Un panneau te donnera la direction si jamais tu sais pas où c est'],
+    [45.184277, 5.722944, 'Une nouvelle fresque. Tu remarqueras la manière dont une cheminée est intégrée au dessin. N oubliez pas le guide merci.'],
+    [45.183632, 5.722256, 'Bon ok je t ai fait faire un tour de manière totalement gratuite. Et puis les chiffres affichés auront peut-être changé d ici demain...'],
+    [45.183172, 5.722641, 'Je peux même plus dire "Allez cocote". Signé : Lucien'],
+    [45.18307, 5.722793, 'L endroit le plus branchouille de toute la ville !!!']
 ];
-
+//Resume game designer URL: https://dpamar.github.io/tresor20aout/generator.html?places=W1s0NS4xODM0MTgsIDUuNzIxNTYyLCAnx2EgbWFyY2hlIHBldXQt6nRyZSBkZXB1aXMgdG9uIGJ1cmVhdSwgbWFpcyBpbCBmYXVkcmEgc29ydGlyIHBvdXIgbGVzIHN1aXZhbnRzIGRlIHRvdXRlcyBsZXMgZmHnb25zIDstKSddLFs0NS4xODM2NzMsIDUuNzIxMzk0LCAnQm9uIG9rLCBjIGVzdCBqdXN0ZSDgIGP0dOkgamUgbWUgc3VpcyBwYXMgZm91bOkuIE1haXMgamUgdCBhaW1lJ10sWzQ1LjE4NDE5NCwgNS43MjE4NzcsICdVbiBwYW5uZWF1IHRlIGRvbm5lcmEgbGEgZGlyZWN0aW9uIHNpIGphbWFpcyB0dSBzYWlzIHBhcyBv+SBjIGVzdCddLFs0NS4xODQyNzcsIDUuNzIyOTQ0LCAnVW5lIG5vdXZlbGxlIGZyZXNxdWUuIFR1IHJlbWFycXVlcmFzIGxhIG1hbmnocmUgZG9udCB1bmUgY2hlbWlu6WUgZXN0IGludOlncullIGF1IGRlc3Npbi4gTiBvdWJsaWV6IHBhcyBsZSBndWlkZSBtZXJjaS4nXSxbNDUuMTgzNjMyLCA1LjcyMjI1NiwgJ0JvbiBvayBqZSB0IGFpIGZhaXQgZmFpcmUgdW4gdG91ciBkZSBtYW5p6HJlIHRvdGFsZW1lbnQgZ3JhdHVpdGUuIEV0IHB1aXMgbGVzIGNoaWZmcmVzIGFmZmljaOlzIGF1cm9udCBwZXV0Lep0cmUgY2hhbmfpIGQgaWNpIGRlbWFpbi4uLiddLFs0NS4xODMxNzIsIDUuNzIyNjQxLCAnSmUgcGV1eCBt6m1lIHBsdXMgZGlyZSAiQWxsZXogY29jb3RlIi4gU2lnbukgOiBMdWNpZW4nXSxbNDUuMTgzMDcwLCA1LjcyMjc5MywgJ0wgZW5kcm9pdCBsZSBwbHVzIGJyYW5jaG91aWxsZSBkZSB0b3V0ZSBsYSB2aWxsZSAhISEnXV0=
